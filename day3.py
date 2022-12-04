9994f285605d05e9ec4a848adafec7ada5200613
Wesley Lam <wesleylam1b@gmail.com>
2022-12-04 14:16:41 +1100

    day3 part2

--- day3.py
+++ day3.py
@@ -34,6 +34,23 @@ def part1(inputFile):
     score += temp
   return score
 
+def findCommon(l1, l2, l3):
+  s1, s2, s3 = set(*l1), set(*l2), set(*l3)
+  s12 = s1.intersection(s2)
+  s123 = s12.intersection(s3)
+  assert len(s123) == 1, list(s123)
+  return list(s123)[0]
+
+def part2(inputFile):
+  lines = readRaw(inputFile)
+  score = 0
+  assert (len(lines) % 3) == 0, len(lines)
+  for i in range(int(len(lines) / 3)):
+    common = findCommon(lines[i*3], lines[i*3 + 1], lines[i*3 + 2])
+    score += toPrior(common)
+  return score
+
 if __name__ == "__main__":
   inputFile = 'source/day3.txt'
-  print(part1(inputFile))
+  # print(part1(inputFile))
+  print(part2(inputFile))
