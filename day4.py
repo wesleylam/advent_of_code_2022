7a466d6325ae443ab011085ced059dd585510a17
Wesley Lam <wesleylam1b@gmail.com>
2022-12-05 23:44:55 +1100

    day4 part2

--- day4.py
+++ day4.py
@@ -1,10 +1,17 @@
 from util import readRaw
 
+def overlap(section1Raw, section2Raw):
+  t1 = [int(i) for i in section1Raw.split('-')]
+  t2 = [int(i) for i in section2Raw.split('-')]
+  if max(t1) >= min(t2) and max(t2) >= min(t1):
+    return True
+  
+  return False
+
 def fullyContain(section1Raw, section2Raw):
   t1 = [int(i) for i in section1Raw.split('-')]
   t2 = [int(i) for i in section2Raw.split('-')]
   if t1[0] <= t2[0] and t1[1] >= t2[1]:
-    print("yes")
     return True
   return False
 
@@ -12,13 +19,20 @@ def part1(inputFile):
   lines = readRaw(inputFile, splitToken=',')
   score = 0
   for tokens in lines:
-    print(tokens)
     if fullyContain(tokens[0], tokens[1]) or \
       fullyContain(tokens[1], tokens[0]):
       score += 1
   return score
 
+def part2(inputFile):
+  lines = readRaw(inputFile, splitToken=',')
+  score = 0
+  for tokens in lines:
+    if overlap(tokens[0], tokens[1]):
+      score += 1
+  return score
+
 if __name__ == "__main__":
   inputFile = 'source/day4.txt'
-  print(part1(inputFile))
-#   print(part2(inputFile))
+  # print(part1(inputFile))
+  print(part2(inputFile))
