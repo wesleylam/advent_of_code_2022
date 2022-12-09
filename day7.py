12737f0b213c004174381b4560a7dece7ccef970
Wesley Lam <wesleylam1b@gmail.com>
2022-12-09 23:01:31 +1100

    day7 part1

---
+++ day7.py
@@ -0,0 +1,69 @@
+from util import readRaw
+
+def part1(inputFile):
+  lines = readRaw(inputFile, splitToken=' ')
+  
+  levels = {}
+  levelFlag = -1
+  pwd = ""
+  store = {}
+  looking = None
+  for tokens in lines:
+    if tokens[0] == "$" and tokens[1] == "ls": # ignore
+      continue
+    
+    if tokens[0] == "$" and tokens[1] == "cd": # command line 
+      d = tokens[2]
+      
+      if d == "..": # ignore "cd .."
+        levelFlag -= 1
+        pwd = "/".join(pwd.split("/")[:-1])
+        if pwd == "": pwd = "/"
+        continue 
+      levelFlag += 1
+      # update pwd except the first one (root)
+      if d != "/": 
+        pwd += ("/" + d) if pwd != "/" else d
+      else:
+        pwd = "/"
+      
+      looking = pwd
+      if pwd not in store:
+        store[pwd] = []
+      if levelFlag not in levels:
+        levels[levelFlag] = []
+      levels[levelFlag].append(pwd)
+      continue
+    
+    # below must be result of ls
+    size, d = tokens[0], tokens[1]
+    store[looking].append(int(size) if size != "dir" else d)
+    
+  
+  finalSizes = {} 
+  
+  # print(store)
+  # print(levels)
+  for i in reversed(range(len(levels))):
+    levelList = levels[i]
+    for path in levelList:
+      temp = 0
+      for size in store[path]:
+        print(i, path, size)
+        if type(size) != int: 
+          toPath = (path + "/" + size) if path != "/" else "/" + size
+          size = finalSizes[toPath] 
+        temp += size
+      finalSizes[path] = temp
+    
+  # print(finalSizes)
+  return sum([size for p, size in finalSizes.items() if size <= 100000])
+
+
+def part2(inputFile):
+  return part1(inputFile, 14)
+  
+if __name__ == "__main__":
+  inputFile = 'source/day7.txt'
+  print(part1(inputFile))
+  # print(part2(inputFile))
